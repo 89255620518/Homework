d8677b5e1caf6e2c393bc443e38fde92b7727326
unknown <  >
2023-04-27 10:58:09 +0300

    add House work

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ else
 */
 
 //Решение:
-
+/*
 Console.Write("imput your 1st number: ");
 int num_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("imput your 2st number: ");
@@ -47,19 +47,57 @@ int num_3 = Convert.ToInt32(Console.ReadLine());
 
 int max = num_1;
 
-if(num_2 > num_1)
+if(max < num_2) max = num_2;
+
+
+if(max < num_3) max = num_3;
+
+Console.WriteLine("max = " + max);
+*/
+
+// Задача№6
+// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+/*
+   Пример:
+   4 -> да
+  -3 -> нет
+   7 -> нет
+*/
+
+// Решение:
+/*
+Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if(number%2 == 0)
 {
-    Console.WriteLine("Input max = num_2");
+    Console.WriteLine(number + " -> yes ");
 }
 else
 {
-    Console.WriteLine(" max = num_1");
+    Console.WriteLine(number + " -> no");
 }
-if(num_3 > num_1)
+*/
+
+// Задача№8
+// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+/*
+   Пример:
+  5 -> 2, 4
+  8 -> 2, 4, 6, 8
+*/
+// Решение:
+/*
+Console.Write("Input your number: ");
+int number_N = Convert.ToInt32(Console.ReadLine());
+
+for(int current_i = 1;current_i <= number_N; current_i++)
 {
-    Console.WriteLine(" max = num_3");
+    if (current_i % 2 == 0)
+    {
+        Console.Write(current_i + ", ");
+    }
 }
-else
-{
-    Console.WriteLine(" max = num_1");
-}
+*/
--- obj/Debug/net6.0/Домашняя задание.AssemblyInfo.cs
+++ obj/Debug/net6.0/Домашняя задание.AssemblyInfo.cs
@@ -10,13 +10,6 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Домашняя задание")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Домашняя задание")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Домашняя задание")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
